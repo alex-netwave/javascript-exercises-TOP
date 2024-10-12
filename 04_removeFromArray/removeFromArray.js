fc309c36a8fd29b47f04868450e23c3815e2badc
Alexander Mills <alexander.mills@gtri.gatech.edu>
2024-10-11 23:37:37 -0400

    initial commit

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(userArray) {
+    const tempArray = userArray;
+    for (const item of arguments){
+        tempArray.splice(tempArray.indexOf(item),1);
+    }
+    return tempArray;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3))
 
 // Do not edit below this line
 module.exports = removeFromArray;
