fdc1f9ac92b6f4444ec953b2d5d7805c3f578b8c
Alexander Mills <alexander.mills@gtri.gatech.edu>
2024-10-12 18:19:15 -0400

    completed exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureF) {
+  let temperatureC = (temperatureF -32) * 5/9;
+  if (Math.floor(temperatureC) === 0) return 0;
+  else return temperatureC.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureC) {
+  let temperatureF = (temperatureC + 32) * 9/5;
+  if (Math.floor(temperatureF) === 0) return 0;
+  else return temperatureF.toFixed(1);
 };
 
+convertToCelsius(0);
+convertToFahrenheit(0);
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
