eea2626a1f85f0ed83470df6a3e7fd8834c14df0
Alexander Mills <alexander.mills@gtri.gatech.edu>
2024-10-12 17:46:05 -0400

    completed exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,15 @@
 const removeFromArray = function(userArray) {
     const tempArray = userArray;
     for (const item of arguments){
-        tempArray.splice(tempArray.indexOf(item),1);
+        if (typeof(item) == "object")
+            continue;
+        let i = tempArray.indexOf(item);
+        tempArray.splice(i,1);
     }
     return tempArray;
 };
 
-console.log(removeFromArray([1, 2, 3, 4], 3))
+console.log(removeFromArray([1, 2, 3, 4], 1))
 
 // Do not edit below this line
 module.exports = removeFromArray;
