91756a3be69a6db1baaa62e3c8a96ecff163261e
Alexander Mills <alexander.mills@gtri.gatech.edu>
2024-10-12 18:01:17 -0400

    completed exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year%100) {
+        if(year%400) return true;
+        else return false;
+    }
+    if (year%4) return true;
 };
 
 // Do not edit below this line
